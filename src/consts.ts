562d644ac2459de53a9acad356df450e80246415
nibtr <nhthieu16@proton.me>
2025-10-23 19:38:23 +0700

    wip: index page

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+import type { Site } from "@types";
+
+export const SITE: Site = {
+  title: 'astro-erudite',
+  description:
+    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'jktrn',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
